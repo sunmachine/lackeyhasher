a9e59c26f1f130404a08c27ea6bffa48df64bb49
Dan Peavey <danpeavey@gmail.com>
2020-03-10 23:54:26 -0700

    Updating package notes.

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setup(
     name="lackeyhash",
     version="0.1.0",
     description="Custom hash function used by LackeyCCG.",
-    long_description="Custom hash function written by Trevor over at LackeyCCG. This is a simple reimplementation in C and packaged as a python module.",
+    long_description="Reimplementation of Lackey's file hashing routine in C as a python module.",
     author="Dan Peavey",
     author_email="danpeavey@gmail.com",
-    url="https://github.com/sunmachine/duelworks-lackey/master/build/hashmodule/",
+    url="https://github.com/sunmachine/lackeyhasher/",
     ext_modules=cythonize(hashmodule_extensions, language_level="3"),
 )
